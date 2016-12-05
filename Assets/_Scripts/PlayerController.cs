17e294c9654b7f84d82dd103e9b285a7a0375dc9
lethalmongoose <gcooper@eng.utah.edu>
2016-12-04 21:05:25 -0700

    Added a fire particle to the fireplace that can be toggled

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject firePS;
+    public bool fireEnabled = true;
+
+	// Use this for initialization
+	void Start () {
+
+        toggleFire();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+            Application.Quit();
+#endif
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            fireEnabled = !fireEnabled;
+            toggleFire();
+
+        }
+	}
+
+    void toggleFire()
+    {
+        foreach (ParticleSystem ps in firePS.GetComponentsInChildren<ParticleSystem>())
+        {
+            if (!fireEnabled)
+                ps.Stop();
+            else
+                ps.Play();
+        }
+        foreach (Light l in firePS.GetComponentsInChildren<Light>())
+        {
+            if (!fireEnabled)
+            {
+                l.gameObject.SetActive(false);
+
+            }
+            else
+                l.gameObject.SetActive(true);
+        }
+    }
+}
