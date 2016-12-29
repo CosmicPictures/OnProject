6a862d075adcbe4047bcbc6bb47eb8bfbded1ffa
DESKTOP-C4PQOLL\Oculus1 <lethalmongoose@gmail.com>
2016-12-29 09:34:59 -0700

    Added lots of information text, added arduino support, fan and heater implemented

---
+++ Assets/_Scripts/elliselkins/SerialBoardController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports; 
+
+public class SerialBoardController : MonoBehaviour {
+	public delegate void MsgReceived(string msg);
+	public event MsgReceived OnMsgReceived;
+
+
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+	protected SerialPort sp = new SerialPort("/dev/cu.usbserial-A600ahi9", 9600);
+#else
+	protected SerialPort sp = new SerialPort("COM7", 9600);
+#endif
+	protected string bufferStr = "";
+
+	// Use this for initialization
+	protected void Start () {
+		startReading();
+	}
+
+	public void sendMessage(string msg)
+	{
+		if (!sp.IsOpen)
+			return;
+
+		sp.Write(msg);
+		Debug.Log("Sending msg: " +  msg);
+	}
+
+	protected void startReading()
+	{
+		try {
+			sp.ReadTimeout = 2;
+			sp.Open();
+			InvokeRepeating("readData", 0.001f, .008f);
+		}
+		catch(System.Exception e) {
+			Debug.Log ("connection failed to arduino: " + e.Message);
+		}
+	}
+
+	protected void readData()
+	{
+		if (!sp.IsOpen)
+			return;
+
+		bool error = false;
+
+		while(!error)
+		{
+			try {
+				char c = (char)sp.ReadChar();
+				bufferStr += c;
+				if (c == '\n') {
+					dispatchMsg();
+				}
+			}
+			catch(System.Exception) {
+				error = true;
+			}
+		}
+	}
+
+	protected void dispatchMsg()
+	{
+		if(OnMsgReceived != null) {
+			OnMsgReceived(bufferStr);
+		}
+		//Debug.Log("Msg received: " + bufferStr);
+		bufferStr = "";
+	}
+}
---
+++ Assets/_Scripts/elliselkins/SerialCommunicationExample.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class SerialCommunicationExample : MonoBehaviour {
+
+	public SerialBoardController sbc;
+	//public Light spotlight;
+
+	// Use this for initialization
+	void Awake () {
+	    //sbc.OnMsgReceived += OnMsgReceived;
+	}
+
+    protected void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            toggleFan();
+        }
+        else if(OVRInput.GetDown(OVRInput.Button.Three))
+        {
+            toggleHeater();
+        }
+    }
+
+    protected bool fanOn;
+    protected bool heaterOn;
+
+    public void toggleFan()
+	{
+		if (fanOn) {
+			sbc.sendMessage("0");
+			fanOn = false;
+		}
+		else {
+			sbc.sendMessage("1");
+			fanOn = true;
+		}
+    }
+
+    public void toggleHeater()
+    {
+        if (heaterOn)
+        {
+            sbc.sendMessage("2");
+            heaterOn = false;
+        }
+        else
+        {
+            sbc.sendMessage("3");
+            heaterOn = true;
+        }
+    }
+
+    //protected void OnMsgReceived(string msg)
+    //{
+    //	//Debug.Log(msg);
+    //	//return;
+    //	float value;
+
+    //	try {
+    //		value = float.Parse(msg);
+    //		value = value / 600 * 8;
+    //		spotlight.intensity = value;
+    //	}
+    //	catch { }
+
+    //}
+}
